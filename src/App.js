e2fb3485b0a8dc25159573a7db4ce7c03f572bee
Richard <olugbenga.rich@gmail.com>
2023-05-26 05:27:22 +0100

    .

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Home from './components/Home';
 import './components/style.css';
-import Movie from './components/Movie';
+
 
 function App() {
   return (
